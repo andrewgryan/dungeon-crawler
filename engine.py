e9ac223ba85d02fbfd7c716846fd46cbca331062
andrewgryan <andrew.ryan@metoffice.gov.uk>
2025-08-01 20:55:25 +0100

    integrate render system

--- engine.py
+++ engine.py
@@ -5,29 +5,6 @@ from curses import wrapper
 
 
 @dataclass
-class World:
-    w: int
-    h: int
-
-    def __post_init__(self):
-        self.buffer = [" "] * (self.w * self.h)
-
-    def clear(self):
-        for i in range(self.w):
-            for j in range(self.h):
-                self.buffer[i + self.w * j] = "."
-
-    def render(self, x, y, char):
-        self.buffer[x + self.w * y] = char
-
-    def draw(self):
-        """Write buffer to screen"""
-        for j in range(self.h):
-            row = "".join(self.buffer[j*self.w:(j+1)*self.w])
-            print(row)
-
-
-@dataclass
 class Position:
     x: int
     y: int
@@ -61,9 +38,28 @@ class Entity:
         return self
 
 
+class RenderSystem:
+    def __init__(self, stdscr, width: int, height: int):
+        self.stdscr = stdscr
+        y, x = 2, 2
+        self.window = curses.newwin(height + 1, width + 1, y, x)
+        self.width = width
+        self.height = height
+
+    def erase(self):
+        self.window.erase()
+
+    def render(self, x, y, c):
+        self.window.addch(y, x, c)
+
+    def refresh(self):
+        self.window.refresh()
+        self.stdscr.refresh()
+
+
 @dataclass
 class Game:
-    world: World
+    render_system: RenderSystem
     entities: list[Entity] = field(default_factory=list)
 
     def with_entity(self):
@@ -72,56 +68,23 @@ class Game:
         return entity
 
     def loop(self):
-        self.world.clear()
-
+        self.render_system.erase()
         for position, renderable in self.iter_traits(Position, Renderable):
-            self.world.render(position.x, position.y, renderable.char)
-
-        self.world.draw()
+            self.render_system.render(position.x, position.y, renderable.char)
+        self.render_system.refresh()
 
     def iter_traits(self, *traits):
         for entity in self.entities:
             if entity.has(*traits):
                 yield entity.get(*traits)
 
-def draw(stdscr, window, width, height, px, py):
-    window.erase()
-    for i in range(0, width):
-        window.addch(0, i, ord('#'))
-        window.addch(height - 1, i, ord('#'))
-    stdscr.refresh()
-    for j in range(0, height):
-        window.addch(j, 0, ord('#'))
-        window.addch(j, width - 1, ord('#'))
-    window.addch(py, px, "@")
-    window.refresh()
-    stdscr.refresh()
-
 
 def main(stdscr):
     stdscr.clear()
     curses.curs_set(False)
-    height, width, y, x = 30, 40, 2, 2
-    window = curses.newwin(height + 1, width + 1, y, x)
+    render_system = RenderSystem(stdscr, width=40, height=30)
 
-    px, py = 12, 12
-    while True:
-        draw(stdscr, window, width, height, px, py)
-        key = stdscr.getkey()
-        if key == "q":
-            return
-        elif key == "h":
-            px -= 1
-        elif key == "j":
-            py += 1
-        elif key == "k":
-            py -= 1
-        elif key == "l":
-            px += 1
-
-    return
-
-    game = Game(World(40, 30))
+    game = Game(render_system)
 
     # Wall
     for i in range(40):
@@ -135,10 +98,25 @@ def main(stdscr):
     player = game.with_entity() + Player() + Position(20, 15) + Renderable("@")
 
     # Movement
-    for _, position in game.iter_traits(Player, Position):
-        position.x += 10
+    while True:
+        key = stdscr.getkey()
+        if key == "q":
+            return
+        elif key == "h":
+            for _, position in game.iter_traits(Player, Position):
+                position.x -= 1
+        elif key == "j":
+            for _, position in game.iter_traits(Player, Position):
+                position.y += 1
+        elif key == "k":
+            for _, position in game.iter_traits(Player, Position):
+                position.y -= 1
+        elif key == "l":
+            for _, position in game.iter_traits(Player, Position):
+                position.x += 1
+        game.loop()
 
-    game.loop()
+    return
 
 
 if __name__ == "__main__":
